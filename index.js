ae99ac872a547125e305a232dcdaecacbd91d692
naknamu <jodeldelvalle@gmail.com>
2024-02-16 13:52:33 +0800

    Added config to connect to mongodb database

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+const createError = require("http-errors");
+const mongoose = require("mongoose");
+
+// Enable env variables
+require("dotenv").config();
 
 // Create an instance of the express app
 const app = express();
+
+// Connect to mongodb
+const { config } = require("./config");
+let mongoDB = config.mongoURI;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(mongoDB)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    }).catch((err) => {
+        console.error("Error connecting to MongoDB:", err.message);
+    });
 
 // Set up middleware
 app.use(morgan("dev")); // logs requests to the console
@@ -15,6 +32,22 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", function(req, res) {
     res.send("Hello World!");
 });
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+})
+
+// Error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json({ error: err});
+})
 
 // Start the server
 const PORT = 3000;
