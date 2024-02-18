de7955f84f8dca8075c4c497478cd0bae17c5a95
naknamu <jodeldelvalle@gmail.com>
2024-02-18 13:10:45 +0800

    Added initial controller setup

---
+++ controllers/imageController.js
@@ -0,0 +1,53 @@
+const Image = require("../models/image");
+const asyncHandler = require("express-async-handler");
+
+// Display list of all images.
+image_list = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: image list");
+});
+
+// Display detail page for a specific image.
+image_detail = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: image detail: ${req.params.id}`);
+})
+
+// Display image create form on GET.
+image_create_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: image create GET");
+});
+
+// Handle image create on POST.
+image_create_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: image create POST");
+});
+
+// Display image delete form on GET.
+image_delete_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: image delete GET");
+});
+
+// Handle image delete on POST.
+image_delete_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: image delete POST");
+});
+
+// Display image update form on GET.
+image_update_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: image update GET");
+});
+
+// Handle image update on POST.
+image_update_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: image update POST");
+});
+
+module.exports = {
+    image_list,
+    image_detail,
+    image_create_get,
+    image_create_post,
+    image_delete_get,
+    image_delete_post,
+    image_update_get,
+    image_update_post,
+}  
---
+++ controllers/uploaderController.js
@@ -0,0 +1,53 @@
+const Uploader = require("../models/uploader");
+const asyncHandler = require("express-async-handler");
+
+// Display list of all uploaders.
+uploader_list = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: uploader list");
+});
+
+// Display detail page for a specific uploader.
+uploader_detail = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: uploader detail: ${req.params.id}`);
+})
+
+// Display uploader create form on GET.
+uploader_create_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: uploader create GET");
+});
+
+// Handle uploader create on POST.
+uploader_create_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: uploader create POST");
+});
+
+// Display uploader delete form on GET.
+uploader_delete_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: uploader delete GET");
+});
+
+// Handle uploader delete on POST.
+uploader_delete_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: uploader delete POST");
+});
+
+// Display uploader update form on GET.
+uploader_update_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: uploader update GET");
+});
+
+// Handle uploader update on POST.
+uploader_update_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: uploader update POST");
+});
+
+module.exports = {
+    uploader_list,
+    uploader_detail,
+    uploader_create_get,
+    uploader_create_post,
+    uploader_delete_get,
+    uploader_delete_post,
+    uploader_update_get,
+    uploader_update_post,
+}  
