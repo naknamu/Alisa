5e45a4655d530fa83e15c28a0d3bb07d5b27e76c
naknamu <jodeldelvalle@gmail.com>
2024-02-21 07:52:38 +0800

    Changed port

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.use(function(err, req, res, next) {
 })
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
