4a861b5eab4aab56ea6c99bdc2a434ddedf91b11
naknamu <jodeldelvalle@gmail.com>
2024-02-20 14:26:32 +0800

    Added CRUD operation for category controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,14 +1,19 @@
 const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all categories.
 category_list = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category list");
+    const results = await Category.find({}, "name").exec();
+
+    res.status(200).json(results);
 });
 
 // Display detail page for a specific category.
 category_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Category detail: ${req.params.categoryname}`);
+    const results = await Category.findOne({ slug: req.params.categoryname }).exec();
+
+    res.status(200).json(results);
 })
 
 // Display category create form on GET.
@@ -17,9 +22,28 @@ category_create_get = asyncHandler(async (req, res, next) => {
 });
 
 // Handle category create on POST.
-category_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category create POST");
-});
+category_create_post = [
+
+    // Validate and sanitize fields.
+    body("name", "Name must not be empty.").trim().isLength({ min: 1 }),
+
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a category object with escaped and trimmed data.
+        const category = new Category({
+            name: req.body.name,
+        });
+
+        if (!errors.isEmpty()) {
+            res.status(400).json(errors.mapped());
+        } else {
+            await category.save();
+            res.status(200).json({ message: `Successfully saved ${req.body.name}` });
+        }
+    }),
+];
 
 // Display category delete form on GET.
 category_delete_get = asyncHandler(async (req, res, next) => {
@@ -28,18 +52,56 @@ category_delete_get = asyncHandler(async (req, res, next) => {
 
 // Handle category delete on POST.
 category_delete_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category delete POST");
+    const category = await Category.findByIdAndDelete(req.params.categoryid);
+
+    res.status(200).json({ message: `Deleted Category: ${category.name}` });
 });
 
 // Display category update form on GET.
 category_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category update GET");
+    const category = await Category.findById(req.params.categoryid);
+
+    res.status(200).json(category);
 });
 
 // Handle category update on POST.
-category_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category update POST");
-});
+category_update_post = [
+
+    // Validate and sanitize fields.
+    body("name", "Name must not be empty.").trim().isLength({ min: 1 }),
+
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a category object with escaped and trimmed data.
+        const category = new Category({
+            name: req.body.name,
+            _id: req.params.categoryid,
+        });
+
+        if (!errors.isEmpty()) {
+            res.status(400).json(errors.mapped());
+        } else {
+        const updatedCategory = await Category.findByIdAndUpdate(
+            req.params.categoryid,
+            category,
+            {
+            new: true, // to return the updated document
+            runValidators: true, // to ensure that any validation rules are applied.
+            context: "query", //  to ensure that the pre-save middleware is triggered
+            }
+        );
+    
+        // Wait for the update to complete
+        await updatedCategory.save();
+    
+        res
+            .status(200)
+            .json({ message: `Successfully updated ${updatedCategory.name}` });
+        }
+    }),
+];
 
 module.exports = {
     category_list,
