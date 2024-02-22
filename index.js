8d8a54c18699c6eb6618b0a179684b40318055d1
naknamu <jodeldelvalle@gmail.com>
2024-02-22 16:59:19 +0800

    Reverted to port 3000

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.use(function(err, req, res, next) {
 })
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
