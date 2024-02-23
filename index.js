783900c6fee168893c5a108d0a019ea3e3bac985
naknamu <jodeldelvalle@gmail.com>
2024-02-23 15:48:21 +0800

    Disabled rate limiter for dev purposes

--- index.js
+++ index.js
@@ -31,15 +31,15 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Set up rate limiter: maximum of 100 requests per 15 minute
-const RateLimit = require("express-rate-limit");
-const limiter = RateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
+// const RateLimit = require("express-rate-limit");
+// const limiter = RateLimit({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+// 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+// 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// });
 // Apply rate limiter to all requests
-app.use(limiter);
+// app.use(limiter);
 
 // Set up middleware
 app.use(compression()); // Compress all routes
