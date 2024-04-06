dbbcb7b77920669df44a8b2baac521a900bbfd1c
naknamu <jodeldelvalle@gmail.com>
2024-04-06 11:30:11 +0800

    Added cron job to automatically delete a comment with no replies every 6:00 on Sunday

--- cronJob.js
+++ cronJob.js
@@ -6,7 +6,7 @@ const deleteComments = async () => {
 };
 
 module.exports = () => {
-  cron.schedule("* * * * *", () => {
+  cron.schedule("0 6 * * Sun", () => {
     console.log("running a task at 6:00 AM every Sunday");
     // Delete comments with flag isDeleted true
     deleteComments();
